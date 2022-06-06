e21114202b89486dae92fa314c9a2091d8cd05b1
slycreator <chinonso1amaechi@gmial.com>
2022-06-06 23:56:16 +0100

    refactor transaction

--- src/decorators/auth-user-decorator.ts
+++ src/decorators/auth-user-decorator.ts
@@ -1,5 +1,9 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { isNumber } from 'class-validator';
 
+interface  IAuthUser {
+  userId?: number
+}
 export const AuthUser = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
--- src/module/transaction/transaction.controller.ts
+++ src/module/transaction/transaction.controller.ts
@@ -1,26 +1,32 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode, HttpStatus, UseGuards } from '@nestjs/common';
 import { TransactionService } from './transaction.service';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { UpdateTransactionDto } from './dto/update-transaction.dto';
 import { AuthUser } from '../../decorators/auth-user-decorator';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @Controller('transactions')
 export class TransactionController {
   constructor(private readonly transactionService: TransactionService) {}
 
   @Post('/deposit')
+  @HttpCode(HttpStatus.OK)
+  @UseGuards(JwtAuthGuard)
   deposit(@AuthUser() authUser, @Body() createTransactionDto: CreateTransactionDto) {
     return this.transactionService.create(authUser,createTransactionDto);
   }
 
-  @Post('/verify-payment')
-  verifyPayment(@Body() createTransactionDto: CreateTransactionDto) {
-  //  return this.transactionService.create(createTransactionDto);
+
+  @Post('/withdraw')
+  @HttpCode(HttpStatus.OK)
+  @UseGuards(JwtAuthGuard)
+  withdrawPayment(@AuthUser() authUser,@Body() createTransactionDto: CreateTransactionDto) {
+    return this.transactionService.createWithdrawal(authUser,createTransactionDto);
   }
 
-  @Post()
-  withdrawPayment(@AuthUser() authUser,@Body() createTransactionDto: CreateTransactionDto) {
-    return this.transactionService.create(authUser,createTransactionDto);
+  @Patch(':id/verify')
+  update(@Param('id') id: string, @Body() updateTransactionDto: UpdateTransactionDto) {
+    return this.transactionService.update(+id, updateTransactionDto);
   }
 
 
