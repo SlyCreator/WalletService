c0fa492b21746eb733472abd2b2ec405505a5894
slycreator <chinonso1amaechi@gmial.com>
2022-06-08 06:54:19 +0100

    added auth test

--- src/module/auth/auth.controller.ts
+++ src/module/auth/auth.controller.ts
@@ -1,7 +1,19 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UsePipes, ValidationPipe } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  UsePipes,
+  ValidationPipe,
+  HttpStatus, HttpCode,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignupDto } from './dto/signup.dto';
 import { LoginDto } from './dto/login.dto';
+import { STATUS_CODES } from 'http';
 
 @Controller('auths')
 export class AuthController {
@@ -9,10 +21,12 @@ export class AuthController {
 
   @UsePipes(ValidationPipe)
   @Post('/register')
+  @HttpCode(HttpStatus.CREATED)
   create(@Body() signupDto: SignupDto) {
     return this.authService.create(signupDto);
   }
   @UsePipes(ValidationPipe)
+  @HttpCode(HttpStatus.OK)
   @Post('/login')
   login(@Body() loginDto:LoginDto) {
     return this.authService.login(loginDto);
